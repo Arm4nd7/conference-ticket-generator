d241595fbc5238cda7bc4d3327ddc97fec393a0c
Arm4nd7 <armando.estevezt@gmail.com>
2025-06-08 13:30:32 -0500

    agregando js para errores de ingreso formulario

--- src/script.js
+++ src/script.js
@@ -1,48 +1,45 @@
 const uploadImageButton = document.getElementById("fileInput");
 let countButtons = 0;
 
-
-
-
 if (uploadImageButton) {
     uploadImageButton.addEventListener("change", () => {
         const borrar = document.getElementById("option-upload");
         borrar?.remove();
-        const label = document.querySelector(".img-upload")
+        const label = document.querySelector(".img-upload");
+        //invalido la funcion de subir archivo en este boton
         label.setAttribute("for", "not-use");
         countButtons++;
         showImage();
         if (countButtons == 1) {
             createButtonModify();
         } else {
-            return
+            return;
         }
-
         buttonRemoveAvatar();
-
-
     });
 }
 
 
 function showImage() {
+    //acceso solo al primer archivo seleccionado [0]
     const file = uploadImageButton.files[0];
+    //nos ayuda a seleccionar (leer)archivos locales
     const reader = new FileReader();
+    //cuando se termina de leer ese archivo lo verifica el onload
     reader.onload = function (event) {
         const label = document.querySelector(".img-upload")
         if (label) {
             const imageUp = document.querySelector(".img-upload img");
             imageUp.className = "newImage";
-            imageUp.src = event.target.result;
+            //contiene el archivo seleccionado (leido)
+            imageUp.src = event.target.result; // ponemos nuestra imagen como url en src
         }
-
     };
     if (!file) {
         alert("No ha seleccionado nada.");
-        return
+        return;
     }
-
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file);//convierte la imagen en una URL
 }
 
 function createButtonModify() {
@@ -55,6 +52,7 @@ function createButtonModify() {
     let editAvatar = document.createElement("label");
     editAvatar.classList.add("editAvatar");
     editAvatar.type = "button";
+    //cambio de funcion subir archivo en este boton
     editAvatar.textContent = "Change image";
     editAvatar.setAttribute("for", "fileInput");
 
@@ -77,6 +75,22 @@ function buttonRemoveAvatar() {
 
 const generateTicketButton = document.getElementById("generateTicketBtn");
 
+function errorEmail() {
+    let label = document.getElementById("emailUser");
+    let labelError = document.createElement("label");
+    labelError.classList.add("email-user-error");
+    let imageError = document.createElement("i");
+    let errorEmail = document.createElement("p");
+    errorEmail.classList.add("parrafo-error");
+    imageError.classList.add(".icon-info2");
+    imageError.innerHTML = `<img src="/src/assets/images/icon-info.svg"></img>`;
+    errorEmail.innerText = `Please enter a valid email adress`;
+    labelError.appendChild(imageError);
+    labelError.appendChild(errorEmail);
+    label.appendChild(labelError);
+    return;
+}
+
 let numeroTiket = 0
 let almacenarNumero = 0
 if (generateTicketButton) {
@@ -87,23 +101,29 @@ if (generateTicketButton) {
         almacenarNumero = numeroTiket++
         pNumero.textContent = numeroTiket;
 
-
-
         const fullName = document.querySelector('input[name="fullname"]').value.trim();
         const emailAddress = document.querySelector('input[name="email"]').value.trim();
         const gitHubUsername = document.querySelector('input[name="GitHub"]').value.trim();
 
         // Verificamos que todos los datos esten completos 
-        if (!fullName || !emailAddress || !gitHubUsername) {
+        if (!fullName || !emailAddress.includes("@") || !emailAddress.includes(".") || !gitHubUsername) {
             alert("Por favor, completa todos los campos.");
+            if(numeroTiket == 1) {
+                errorEmail()
+            }else{
+                return;
+            }
             return;
         }
 
         // Imagen actualizada en el tiket
-        let uploadedImageSrc = "./assets/images/image-avatar.jpg";
+        let uploadedImageSrc = document.querySelector(".user-logo");
         const imageIconUpload = document.querySelector(".img-upload img");
-        if (imageIconUpload && imageIconUpload.src && !imageIconUpload.src.includes("icon-upload.svg")) {
-            uploadedImageSrc = imageIconUpload.src;
+        if (!imageIconUpload.src.includes("icon-upload.svg")) {
+            uploadedImageSrc.src = imageIconUpload.src;
+        } else {
+            alert("Escoja una imagen");
+            return;
         }
 
         // Guardamos en localStorage
@@ -111,23 +131,19 @@ if (generateTicketButton) {
         localStorage.setItem("ticketEmailAddress", emailAddress);
         localStorage.setItem("ticketGitHubUsername", gitHubUsername);
         localStorage.setItem("ticketImageSrc", uploadedImageSrc);
-
-
-
         // ponemos los datos del ticket directamente
         document.getElementById("ticketTitleName").textContent = fullName;
         document.getElementById("ticketTitleEmail").textContent = emailAddress;
         document.getElementById("ticketUserName").textContent = fullName;
         document.getElementById("ticketUserEmail").textContent = emailAddress;
-
-                // Mostramos la pantalla del ticket
+        // Mostramos la pantalla del ticket
         document.querySelector("header").style.display = "none";
         document.querySelector("main").style.display = "none";
         document.getElementById("main-2").style.display = "block";
 
-        const userLogo = document.querySelector(".user-logo");
-        if (userLogo && uploadedImageSrc) {
-            userLogo.src = uploadedImageSrc;
-        }
+
+
+
+
     });
 }
